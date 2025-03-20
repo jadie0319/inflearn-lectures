d68aa3d104603a93d964d61b99f85068ea11f00e
Jadie <koola976@gmail.com>
2025-03-20 09:58:42 +0900

    Initial commit

---
+++ src/main/kotlin/jadie/inflearnlectures/InflearnLecturesApplication.kt
@@ -0,0 +1,11 @@
+package jadie.inflearnlectures
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class InflearnLecturesApplication
+
+fun main(args: Array<String>) {
+    runApplication<InflearnLecturesApplication>(*args)
+}
---
+++ src/test/kotlin/jadie/inflearnlectures/InflearnLecturesApplicationTests.kt
@@ -0,0 +1,13 @@
+package jadie.inflearnlectures
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class InflearnLecturesApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
