9ee5119cb34b85c1029641cbf823a4efedbaae20
Jadie <koola976@gmail.com>
2025-03-20 10:05:43 +0900

    feat: 모듈 추가 테스트

---
+++ concur-lecture/src/main/java/jadie/concurlecture/ConcurLectureApplication.java
@@ -0,0 +1,13 @@
+package jadie.concurlecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurLectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcurLectureApplication.class, args);
+	}
+
+}
---
+++ concur-lecture/src/test/java/jadie/concurlecture/ConcurLectureApplicationTests.java
@@ -0,0 +1,13 @@
+package jadie.concurlecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcurLectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
